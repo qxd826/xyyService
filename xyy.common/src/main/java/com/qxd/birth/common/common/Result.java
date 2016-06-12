3e6bf9993218095a12ae02ef5ca5160fc7270951
xiangdong.qu <qxd826@163.com>
2016-06-12 23:56:56 +0800

    xyyy

---
+++ xyy.common/src/main/java/com/qxd/birth/common/common/Result.java
@@ -0,0 +1,94 @@
+package com.qxd.birth.common.common;
+
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+/**
+ * Created by xiangDong.qu on 15/10/26.
+ * 返回结果
+ */
+@EqualsAndHashCode
+@ToString
+public class Result<T> {
+    /**
+     * 消息码
+     */
+    private String code;
+
+    /**
+     * 返回数据
+     */
+    private T data;
+
+    /**
+     * 错误信息
+     */
+    private String message;
+
+    /**
+     * 是否成功 标志
+     */
+    private boolean success;
+
+    public Result() {
+    }
+
+    public Result(String message) {
+        this.message = message;
+    }
+
+    public String getErrorMsg() {
+        return message;
+    }
+
+    public void setErrorMsg(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public static <T> Result<T> wrapSuccessfulResult(T data) {
+        Result<T> result = new Result();
+        result.data = data;
+        result.success = true;
+        return result;
+    }
+
+    public static <T> Result<T> wrapErrorResult(BirthError error) {
+        Result<T> result = new Result();
+        result.success = false;
+        result.code = error.getCode();
+        result.message = error.getMessage();
+        return result;
+    }
+
+    public static <T> Result<T> wrapErrorResult(String code, String message) {
+        Result<T> result = new Result<T>();
+        result.success = false;
+        result.code = code;
+        result.message = message;
+        return result;
+    }
+}
